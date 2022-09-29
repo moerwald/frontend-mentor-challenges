979152d5a2912533e21b3e139f03d640424d9fb3
moerwald <andreas.mewald@gmail.com>
2022-09-29 23:00:30 +0200

    Chosen rating.
    Expanded JS so that chosen rating is stored. After button click the
    thank you section if shown. Open: Styling of thank you section.

--- challenges/interactive-rating-component-main/main.js
+++ challenges/interactive-rating-component-main/main.js
@@ -1,27 +1,44 @@
+"use strict";
 
-var ratingIcons = document.getElementsByClassName("rating-option");
-var button = document.getElementsByTagName("button")[0];
-var currentRating = undefined;
+(function () {
 
-button.addEventListener("click", (event) => {
-    console.log("CLICKED");
-    console.log(event);
-});
+    var ratingIcons = document.getElementsByClassName("rating-option");
+    var button = document.getElementsByTagName("button")[0];
+    var currentRating = "empty";
+    button.disabled = true;
 
-for (var i of ratingIcons) {
-    i.addEventListener("click", (event) => {
+    console.log(currentRating);
 
+    button.addEventListener("click", (event) => {
+        console.log(currentRating);
 
-        var ratingElement = event.target;
-        const ratingClickedClass = "rating-clicked";
+        var ratingSection = document.getElementsByClassName("rating_section")[0];
+        var thankYouSection = document.getElementsByClassName("thank_you_section")[0];
+        var chosenRating = document.getElementById("chosen_rating");
 
-        let foundElements = Array.from(ratingIcons).filter(ri => ri.classList.contains(ratingClickedClass));
-        if (foundElements.length > 0){
-            foundElements.forEach(e => e.classList.remove(ratingClickedClass));
-        }
-
-        ratingElement.classList.add(ratingClickedClass);
-        currentRating = ratingClickedClass.innerText;
-
+        ratingSection.classList.add("display-no");
+        thankYouSection.classList.remove("display-no");
+        chosenRating.innerText = currentRating;
     });
-}
+
+    for (var i of ratingIcons) {
+        i.addEventListener("click", (event) => {
+
+            var ratingElement = event.target;
+            const ratingClickedClass = "rating-clicked";
+
+            let foundElements = Array.from(ratingIcons).filter(ri => ri.classList.contains(ratingClickedClass));
+            if (foundElements.length > 0) {
+                foundElements.forEach(e => e.classList.remove(ratingClickedClass));
+            }
+
+            ratingElement.classList.add(ratingClickedClass);
+
+            currentRating = ratingElement.innerText;
+            button.disabled = false;
+
+            console.log(currentRating);
+
+        });
+    }
+})();

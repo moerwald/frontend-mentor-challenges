e6e626f74036b7ede9014d5d37eaef6eb43ca205
moerwald <andreas.mewald@gmail.com>
2022-10-21 10:27:25 +0200

    article-preview-component-master mobile view
    Colors of share button are now changing on click.

--- challenges/article-preview-component-master/main.js
+++ challenges/article-preview-component-master/main.js
@@ -5,11 +5,26 @@ button.addEventListener("click", () => {
     const section = document.getElementById("social-media-banner");
     const socialMediaHidden = "social-media-hidden";
     const socialMediaShowed = "social-media-show";
+    const shareButtonIsActive = "share-button-activated";
+
+    const updateShareButtonColors = function updateShareBtnColors(btn){
+        const iconOfShareButton = document.getElementById("share-button-icon");
+        if (iconOfShareButton.classList.contains(shareButtonIsActive)) {
+            iconOfShareButton.classList.remove(shareButtonIsActive);
+            btn.classList.remove(shareButtonIsActive);
+            return;
+        }
+        iconOfShareButton.classList.add(shareButtonIsActive);
+        btn.classList.add(shareButtonIsActive);
+    };
 
     const hiddenClassSet = section.classList.contains(socialMediaHidden);
     section.classList.remove(hiddenClassSet ? socialMediaHidden : socialMediaShowed);
     section.classList.add(hiddenClassSet ? socialMediaShowed : socialMediaHidden);
 
+    updateShareButtonColors(button);
+
+    /*
     if (hiddenClassSet) {
         section.appendChild(button);
         return;
@@ -17,6 +32,7 @@ button.addEventListener("click", () => {
 
     const footer = document.getElementsByTagName("footer")[0];
     footer.appendChild(button);
+    */
 });
 
 

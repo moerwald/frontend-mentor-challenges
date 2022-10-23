a991ca379b81af3748d75a9d9797c0e1a2ded4aa
moerwald <andreas.mewald@gmail.com>
2022-10-23 12:12:13 +0200

    article-preview-component-master Javascript
    Simplified javascript code.

--- challenges/article-preview-component-master/main.js
+++ challenges/article-preview-component-master/main.js
@@ -3,25 +3,13 @@ const button = document.getElementById("share-button");
 
 button.addEventListener("click", () => {
     const section = document.getElementById("social-media-banner");
-    const socialMediaHidden = "social-media-hidden";
     const socialMediaShowed = "social-media-show";
     const shareButtonIsActive = "share-button-activated";
 
-    const updateShareButtonColors = function updateShareBtnColors(btn){
-        // Update button colors
-        if (btn.classList.contains(shareButtonIsActive)) {
-            btn.classList.remove(shareButtonIsActive);
-            return;
-        }
-        btn.classList.add(shareButtonIsActive);
-    };
-
     // Show/Hide social media banner
-    const hiddenClassSet = section.classList.contains(socialMediaHidden);
-    section.classList.remove(hiddenClassSet ? socialMediaHidden : socialMediaShowed);
-    section.classList.add(hiddenClassSet ? socialMediaShowed : socialMediaHidden);
+    section.classList.toggle(socialMediaShowed);
+    button.classList.toggle(shareButtonIsActive);
 
-    updateShareButtonColors(button);
 });
 
 
